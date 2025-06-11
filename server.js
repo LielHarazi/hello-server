a60a10ae8b4c485e77e75b364df050fe35ad5e19
liel harazi <lielharazi@gmail.com>
2025-06-11 14:19:27 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.use(express.static("Public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "Public", "index.html"));
+});
+app.get("/about", (requst, response) => {
+  response.send("bobi");
+});
+
+app.listen(PORT, () => {
+  console.log(`serve listening at http://localhost:${PORT}/`);
+});
