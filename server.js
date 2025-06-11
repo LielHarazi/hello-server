3b14a3470a1e7bdff3fa75c48b8a8a96777012fc
liel harazi <lielharazi@gmail.com>
2025-06-11 15:52:23 +0300

    notfound

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 const express = require("express");
+const { request } = require("http");
 const path = require("path");
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-app.use(express.static("Public"));
-
-app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, "Public", "index.html"));
-});
-app.get("/about", (request, response) => {
-  response.send("bobi");
-});
-
-// 404 handler for any unmatched route
-app.use((request, response) => {
-  response
-    .status(404)
-    .sendFile(path.join(__dirname, "Public", "notFound.html"));
-});
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
-  console.log(`serve listening at http://localhost:${PORT}/`);
+  console.log(`server is listening on http://localhost:${PORT}`);
+});
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.get("/about", (request, response) => {
+  response.sendFile(path.join(__dirname, "public", "about.html"));
+});
+app.use((request, response) => {
+  response.sendFile(path.join(__dirname, "public", "not-found-page.html"));
 });
